de7ec9afc9aa44f0d557592fe1d7e4859514a5ab
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-25 17:21:54 +0200

    Exercise: Result

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -2,17 +2,26 @@
 
 data class User(val name: String, val password: String)
 
+enum class UserError {
+    USERNAME_OUT_OF_BOUNDS,
+    PASSWORD_TOO_SHORT
+}
+
 fun validateName(name: String): Boolean =
     name.isNotEmpty() && name.length <= 15
 
 fun validatePassword(password: String): Boolean =
     password.length >= 10
 
-fun createUser(name: String, password: String): Option<User> =
-    User(name, password).takeIf {
-        validateName(name) && validatePassword(password)
-    }?.let(::Just)
-    ?: None
+fun createUser(name: String, password: String): Result<User, UserError> {
+    if (!validateName(name))
+        return Failure(UserError.USERNAME_OUT_OF_BOUNDS)
+
+    if (!validatePassword(password))
+        return Failure(UserError.PASSWORD_TOO_SHORT)
+
+    return User(name, password).let(::Success)
+}
 
 fun main(args: Array<String>) {
     createUser("Antonio", "functionalrocks")

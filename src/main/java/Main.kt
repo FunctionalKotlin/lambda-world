64857f6c4099c7c470db580e70d6dd3dd795823a
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-25 17:37:42 +0200

    Exercise: Validating with Result

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,38 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-fun main(args: Array<String>) {
-    val a = 3 + 1
+data class User(val name: String, val password: String)
 
-    println(a)
+enum class UserError {
+    USERNAME_OUT_OF_BOUNDS,
+    PASSWORD_TOO_SHORT
+}
+
+fun validateName(name: String): Result<String, UserError> =
+    name.takeIf { name.isNotEmpty() && name.length <= 15 }
+        ?.let(::Success)
+        ?: Failure(UserError.USERNAME_OUT_OF_BOUNDS)
+
+fun validatePassword(password: String): Result<String, UserError> =
+    password.takeIf { password.length >= 10 }
+        ?.let(::Success)
+        ?: Failure(UserError.PASSWORD_TOO_SHORT)
+
+fun createUser(name: String, password: String): Result<User, UserError> {
+    val validateName = validateName(name)
+
+    if (validateName is Failure)
+        return validateName
+
+    val validatePassword = validatePassword(password)
+
+    if (validatePassword is Failure)
+        return validatePassword
+
+    return User(name, password).let(::Success)
+}
+
+fun main(args: Array<String>) {
+    createUser("Antonio", "functionalrocks")
+        .map { it.name }
+        .ifSuccess(::println)
 }
---
+++ src/main/java/Result.kt
@@ -0,0 +1,17 @@
+// Copyright © FunctionalKotlin.com 2017. All rights reserved.
+
+sealed class Result<out A, out E>
+
+data class Success<out A>(val value: A): Result<A, Nothing>()
+
+data class Failure<out E>(val error: E): Result<Nothing, E>()
+
+fun <A, E, B> Result<A, E>.map(transform: (A) -> B): Result<B, E> =
+    when(this) {
+        is Success -> Success(transform(this.value))
+        is Failure -> this
+    }
+
+fun <A> Result<A, *>.ifSuccess(continuation: (A) -> Unit) {
+    if (this is Success) continuation(value)
+}

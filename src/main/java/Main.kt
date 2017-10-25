06605cd74e1c41100c48ad2c62d41ce67133f919
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-25 18:17:50 +0200

    Return to Validators exercise

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,14 +1,38 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-data class Organization(val name: String)
+import Validators.IsValidName
+import Validators.IsValidPassword
 
-data class User(val name: String, val organizationId: Int)
+data class User(val name: String, val password: String)
 
-fun getOrganizationById(id: Int): Option<Organization> = Just(Organization("Functional Kotlin"))
+enum class UserError {
+    USERNAME_OUT_OF_BOUNDS,
+    PASSWORD_TOO_SHORT
+}
 
-fun getUserById(id: Int): Option<User> = Just(User("alex", 1))
+typealias Validator<A, E> = (A) -> Result<A, E>
+
+fun <A> validate(with: (A) -> Boolean): (A) -> A? = { it.takeIf(with) }
+
+fun <A, E> ((A) -> A?).orElseFail(with: E): Validator<A, E> = {
+    this(it)?.let(::Success) ?: Failure(with)
+}
+
+object Validators {
+    val IsValidName: Validator<User, UserError> =
+        validate<User>(with = { it.name.isNotEmpty() && it.name.length < 15 })
+            .orElseFail(with = UserError.USERNAME_OUT_OF_BOUNDS)
+
+    val IsValidPassword: Validator<User, UserError> =
+        validate<User>(with = { it.password.length >= 10 })
+            .orElseFail(with = UserError.PASSWORD_TOO_SHORT)
+}
+
+fun createUser(name: String, password: String): Result<User, UserError> =
+    User(name, password).let(allOf(IsValidName, IsValidPassword))
 
 fun main(args: Array<String>) {
-    val option: Option<Organization> = getUserById(42)
-        .flatMap { user -> getOrganizationById(user.organizationId) }
+    createUser("Antonio", "functionalrocks")
+        .map { it.name }
+        .ifSuccess(::println)
 }
---
+++ src/main/java/Result.kt
@@ -0,0 +1,31 @@
+// Copyright © FunctionalKotlin.com 2017. All rights reserved.
+
+sealed class Result<out A, out E>
+
+data class Success<out A>(val value: A): Result<A, Nothing>()
+
+data class Failure<out E>(val error: E): Result<Nothing, E>()
+
+fun <A, E, B> Result<A, E>.map(transform: (A) -> B): Result<B, E> =
+    when(this) {
+        is Success -> Success(transform(this.value))
+        is Failure -> this
+    }
+
+fun <A> Result<A, *>.ifSuccess(continuation: (A) -> Unit) {
+    if (this is Success) continuation(value)
+}
+
+operator infix fun <T, E>
+    Validator<T, E>.plus(validator: Validator<T, E>): Validator<T, E> = {
+
+    val result = this(it)
+
+    when (result) {
+        is Success -> validator(it)
+        is Failure -> result
+    }
+}
+
+fun <A, E> allOf(vararg validators: Validator<A, E>): Validator<A, E> =
+    validators.fold(::Success) { acc, function -> acc + function }

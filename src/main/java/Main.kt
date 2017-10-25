db5e35fe1fe0e82d412877c8395f636cc6c85658
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-25 16:55:04 +0200

    Exercise: Using nullables

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,19 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-fun main(args: Array<String>) {
-    val a = 3 + 1
+data class User(val name: String, val password: String)
 
-    println(a)
+fun validateName(name: String): Boolean =
+    name.isNotEmpty() && name.length <= 15
+
+fun validatePassword(password: String): Boolean =
+    password.length >= 10
+
+fun createUser(name: String, password: String): User? =
+    User(name, password).takeIf {
+        validateName(name) && validatePassword(password)
+    }
+
+fun main(args: Array<String>) {
+    createUser("Antonio", "functionalrocks")
+        ?.let(::println)
 }

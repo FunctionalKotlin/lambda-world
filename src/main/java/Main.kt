54f6134ad5304ce23992616784e7573f380c8761
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-25 18:05:50 +0200

    Exercise: User & Organization example

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,14 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-fun main(args: Array<String>) {
-    val a = 3 + 1
+data class Organization(val name: String)
 
-    println(a)
+data class User(val name: String, val organizationId: Int)
+
+fun getOrganizationById(id: Int): Option<Organization> = Just(Organization("Functional Kotlin"))
+
+fun getUserById(id: Int): Option<User> = Just(User("alex", 1))
+
+fun main(args: Array<String>) {
+    getUserById(42)
+        .map { user -> getOrganizationById(user.organizationId) }
 }
---
+++ src/main/java/Option.kt
@@ -0,0 +1,15 @@
+// Copyright © FunctionalKotlin.com 2017. All rights reserved.
+
+sealed class Option<out A>
+
+object None : Option<Nothing>()
+data class Just<out A>(val value: A) : Option<A>()
+
+fun <A, B> Option<A>.map(transform: (A) -> B): Option<B> = when(this) {
+    is Just -> Just(transform(this.value))
+    is None -> None
+}
+
+fun <A> Option<A>.ifPresent(continuation: (A) -> Unit) {
+    if (this is Just) continuation(value)
+}
